e617917df52554dc5e6f4d57d8b78aa4d2b301a9
sloth-bear <slothbear.hj@gmail.com>
2021-06-02 21:44:16 +0900

    Added Functions
    종목 편집 기능 추가
    초기화 기능 추가

--- component/app.js
+++ component/app.js
@@ -1,6 +1,5 @@
 const localStorageKey = {
-  account: 'account',
-  desiredEntries: 'desiredEntries'
+  tabs: 'tabs'
 }
 
 const app = new Vue({
@@ -8,8 +7,10 @@ const app = new Vue({
   vuetify: new Vuetify(),
   data () {
     return {
-      account: { ...this.getInitCryptoCurrency() },
-      desiredEntries: [ this.getInitCryptoCurrency() ]
+      activeTabIndex: 0,
+      tabs: [{ ...this.getInitTab() }],
+      isShowTabsSettingDialog: false,
+      checkedTabIndexes: []
     }
   },
   computed: {
@@ -25,7 +26,7 @@ const app = new Vue({
     }
   },
   mounted () {
-    this.initialize()
+    this.initialize(this.getAllStoredTabs)
     this.focusAccountEntryPrice()
   },
   methods: {
@@ -46,23 +47,40 @@ const app = new Vue({
 
       this.saveAll()
     },
+    onClickAddTab () {
+      this.tabs = this.tabs.concat([{ ...this.getInitTab() }])
+    },
+    onClickRemoveTabs () {
+      this.tabs = this.tabs.filter((v, i) => this.checkedTabIndexes.indexOf(i) < 0)
+    },
+    onClickCloseTabSettingDialog () {
+      this.checkedTabIndexes = []
+      this.isShowTabsSettingDialog = false
+    },
+    onClickInitialize () {
+      this.initialize(this.deleteAllStoredTabs)
+    },
     onClickAddDesiredEntry () {
-      this.desiredEntries = [{ ...this.getInitCryptoCurrency() }].concat(this.desiredEntries)
+      this.tabs[this.activeTabIndex].desiredEntries = [{ ...this.getInitCryptoCurrency() }].concat(this.tabs[this.activeTabIndex].desiredEntries)
 
       this.focusDesiredEntry(0)
     },
     onClickRemoveDesiredEntry (index) {
-      this.desiredEntries = this.desiredEntries.filter((v, i) => i !== index)
+      this.tabs[this.activeTabIndex].desiredEntries = this.tabs[this.activeTabIndex].desiredEntries.filter((v, i) => i !== index)
     },
     calculateTotalQuantity () {
-      const accountQuantity = this.parseToInteger(this.account.quantity)
-      const totalDesiredQuantity = this.desiredEntries.reduce((acc, curr) => acc + this.parseToInteger(curr.quantity), 0)
+      const activeTab = this.tabs[this.activeTabIndex]
+
+      const accountQuantity = this.parseToInteger(activeTab.account.quantity)
+      const totalDesiredQuantity = activeTab.desiredEntries.reduce((acc, curr) => acc + this.parseToInteger(curr.quantity), 0)
 
       return accountQuantity + totalDesiredQuantity
     },
     calculateTotalPrice () {
-      const accountEntryPrice = this.parseToInteger(this.account.totalEntryPrice)
-      const desiredTotalEntryPrice = this.desiredEntries.reduce((acc, curr) => acc + this.parseToInteger(curr.totalEntryPrice), 0)
+      const activeTab = this.tabs[this.activeTabIndex]
+
+      const accountEntryPrice = this.parseToInteger(activeTab.account.totalEntryPrice)
+      const desiredTotalEntryPrice = activeTab.desiredEntries.reduce((acc, curr) => acc + this.parseToInteger(curr.totalEntryPrice), 0)
 
       return accountEntryPrice + desiredTotalEntryPrice
     },
@@ -82,28 +100,35 @@ const app = new Vue({
     focusDesiredEntry (index) {
       this.$refs.desiredFirstEntries[index].focus()
     },
-    initialize () {
-      const localStorageKeys = Object.keys(localStorage)
+    initialize (onAfterInitialize = () => {}) {
+      this.activeTabIndex = 0
+      this.tabs = [{ ...this.getInitTab() }]
 
-      if (localStorageKeys.indexOf(localStorageKey.account) > -1) {
-        try {
-          this.account = JSON.parse(localStorage.getItem(localStorageKey.account))
-        } catch (error) {
-          localStorage.removeItem(localStorageKey.account)
-        }
-      }
-
-      if (localStorageKeys.indexOf(localStorageKey.desiredEntries) > -1) {
-        try {
-          this.desiredEntries = JSON.parse(localStorage.getItem(localStorageKey.desiredEntries))
-        } catch (error) {
-          localStorage.removeItem(localStorageKey.desiredEntries)
-         }
-      }
+      onAfterInitialize()
     },
     saveAll () {
-      localStorage.setItem(localStorageKey.account, JSON.stringify(this.account))
-      localStorage.setItem(localStorageKey.desiredEntries, JSON.stringify(this.desiredEntries))
+      localStorage.setItem(localStorageKey.tabs, JSON.stringify(this.tabs))
+    },
+    getAllStoredTabs () {
+      const localStorageKeys = Object.keys(localStorage)
+
+      if (localStorageKeys.indexOf(localStorageKey.tabs) > -1) {
+        try {
+          this.tabs = JSON.parse(localStorage.getItem(localStorageKey.tabs))
+        } catch (error) {
+          this.deleteAllStoredTabs()
+        }
+      }
+    },
+    deleteAllStoredTabs () {
+      localStorage.removeItem(localStorageKey.tabs)
+    },
+    getInitTab () {
+      return {
+        name: '종목명',
+        account: { ...this.getInitCryptoCurrency() },
+        desiredEntries: [ this.getInitCryptoCurrency() ]
+      }
     }
   }
 })
